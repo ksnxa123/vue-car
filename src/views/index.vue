1fdad6011bb90144913a6b8d3074c625b3ce8c87
ksnxa123 <2387052992@qq.com>
2020-09-03 15:28:07 +0800

    项目初始化

---
+++ src/views/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="amap-wrap">
+      <el-amap vid="amapDEmo" :center="center" :zoom="zoom" class="amap-demo"></el-amap>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"Index",
+    data(){
+        return{
+            zoom:12,
+            center:[118.831312,33.55782]
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.amap-wrap{
+    height: 90vh;
+}
+</style>
